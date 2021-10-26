a5b71cbc19618b09bff2389732f8fedaf15a86f7
DonovanContreras <89878545+DonovanContreras@users.noreply.github.com>
2021-10-26 15:54:27 -0500

    Create cyp.py

---
+++ cyp.py
@@ -0,0 +1,7 @@
+print("""
+DDDD   OOOOO  N   N  OOOOO  V       V   A     N   N
+D   D  O   O  NN  N  O   O   V     V   A A    NN  N
+D   D  O   O  N N N  O   O    V   V   A   A   N N N
+D   D  O   O  N  NN  O   O     V V   AAAAAAA  N  NN
+DDDD   OOOOO  N   N  OOOOO      V   A       A N   N
+                                                """)
